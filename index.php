e2733821d1d89144288c43cbd4d35f22e67fedc6
pbernardoss01 <pbernardoss01@educantabria.es>
2020-12-28 19:41:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -21,8 +21,7 @@
       <meta name="author" content="Patricia Bernardos Sobrino">
 
       <!-- Fontawesome -->
-      <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
       <!-- Bootstrap core CSS -->
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
