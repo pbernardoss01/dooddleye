e1cfbc38ccc833a813b11b2d1cc4ac2b6c2a1499
pbernardoss01 <pbernardoss01@educantabria.es>
2020-12-28 19:28:15 +0100

    cambios en index.php

--- index.php
+++ index.php
@@ -21,16 +21,18 @@
       <meta name="author" content="Patricia Bernardos Sobrino">
 
       <!-- Fontawesome -->
-      <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+      <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
       <!-- Bootstrap core CSS -->
-      <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
 
       <!-- Custom styles for this template -->
-      <link href="css/style.css" rel="stylesheet">
+      <link href="/css/style.css" rel="stylesheet">
      
-      <script src="vendor/jquery/jquery.min.js"></script>
-      <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
       <script src="js/lightbox/lightbox-2.6.min.js"></script>
     </head>
     <body>
